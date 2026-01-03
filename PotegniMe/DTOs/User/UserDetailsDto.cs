bdb8b657244b2642822713b466048172af2f8272
Lan <lanlebar6@gmail.com>
2026-01-03 11:53:20 +0100

    GetCurrentUserAsync

--- PotegniMe/DTOs/User/UserDetailsDto.cs
+++ PotegniMe/DTOs/User/UserDetailsDto.cs
@@ -4,7 +4,7 @@ public class UserDetailsDto
 {
     public required string Username { get; set; }
     public required string Email { get; set; }
-    public required DateTime Joined { get; set; }
+    public required DateOnly Joined { get; set; }
     public required string Role { get; set; }
     public required bool HasPfp { get; set; }
 }
