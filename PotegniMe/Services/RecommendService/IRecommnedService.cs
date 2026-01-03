5227cdc1a1a63f8f0ae7985e43a2bdcf56856715
Lan <lanlebar6@gmail.com>
2026-01-03 12:01:08 +0100

    interface cleanup

--- PotegniMe/Services/RecommendService/IRecommnedService.cs
+++ PotegniMe/Services/RecommendService/IRecommnedService.cs
@@ -2,12 +2,8 @@
 
 public interface IRecommendService
 {
-    // Set recommendation
+    // Admin recommendations
     Task<Recommendation> SetRecommendation(Recommendation recommendation);
-
-    // Get recommendation
     Task<Recommendation> GetRecommendation(DateOnly date, string type);
-
-    // Delete recommendation
     Task DeleteRecommendation(DateOnly date, string type);
 }
